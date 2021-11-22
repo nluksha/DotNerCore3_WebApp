08e4413022cea04a320f8d362a237c7a76dc5ae8
Nikita Luksha <nikita.luksha@softensity.com>
2021-11-22 08:50:40 +0300

    Setup EF

---
+++ WebApp/WebApp/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
+{
+    public class Product
+    {
+        public long ProductId { get; set; }
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal Price { get; set; }
+
+        public long CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public long SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+    }
+}
--- WebApp/WebApp/Startup.cs
+++ WebApp/WebApp/Startup.cs
@@ -7,19 +7,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
 
 namespace WebApp
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:ProductConnection"]);
+                options.EnableSensitiveDataLogging(true);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext context)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +49,8 @@ namespace WebApp
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            SeedData.SeedDatabase(context);
         }
     }
 }
