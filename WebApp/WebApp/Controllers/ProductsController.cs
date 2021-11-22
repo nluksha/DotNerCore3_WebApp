b3ad210c6a859b56fb761d01244c024c3278bc76
Nikita Luksha <nikita.luksha@softensity.com>
2021-11-22 20:43:15 +0300

    Refactor controllers

--- WebApp/WebApp/Controllers/ProductsController.cs
+++ WebApp/WebApp/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -10,20 +11,25 @@ namespace WebApp.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private DataContext context;
+
+        public ProductsController(DataContext context)
+        {
+            this.context = context;
+        }
+
         [HttpGet]
         public IEnumerable<Product> GetProducts()
         {
-            return new Product[]
-            {
-                new Product { Name  = "Product #1"},
-                new Product { Name  = "Product #2"}
-            };
+            return context.Products;
         }
 
         [HttpGet("{id}")]
-        public Product GetProduct()
+        public Product GetProduct([FromServices] ILogger<ProductsController> logger)
         {
-            return new Product { ProductId = 1, Name = "Test Product" };
+            logger.LogDebug("GetProduct Action Invoked");
+
+            return context.Products.FirstOrDefault();
         }
     }
 }
