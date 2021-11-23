7d5698c6d20777d73e0398e3b010c13fc76f2cee
Nikita Luksha <nikita.luksha@softensity.com>
2021-11-23 21:00:35 +0300

    EF Inclure data

---
+++ WebApp/WebApp/Controllers/SupplierController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SupplierController : ControllerBase
+    {
+        private DataContext context;
+
+        public SupplierController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Supplier> GetSupplier(long id)
+        {
+            Supplier supploer = await context.Suppliers.Include(s => s.Products).FirstAsync(s => s.SupplierId == id);
+
+            foreach (var p in supploer.Products)
+            {
+                p.Supplier = null;
+            }
+
+            return supploer;
+        }
+    }
+}
