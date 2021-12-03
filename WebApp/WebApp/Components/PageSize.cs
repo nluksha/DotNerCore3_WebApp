6e6e9b401f0afe9cbe413fe15d1186855fc50dbd
Nikita Luksha <nikita.luksha@softensity.com>
2021-12-03 20:13:33 +0300

    Add VC

---
+++ WebApp/WebApp/Components/PageSize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace WebApp.Components
+{
+    public class PageSize: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://apress.com");
+
+            return View(response.Content.Headers.ContentLength);
+        }
+    }
+}
