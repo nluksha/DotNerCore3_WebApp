59640db2654a26292cd0acf423e4176789133852
Nikita Luksha <nikita.luksha@softensity.com>
2021-12-03 18:03:55 +0300

    Add ViewComponents

---
+++ WebApp/WebApp/Components/CitySummary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Components
+{
+    public class CitySummary: ViewComponent
+    {
+        private CitiesData data;
+
+        public CitySummary(CitiesData data)
+        {
+            this.data = data;
+        }
+
+        public string Invoke()
+        {
+            return $"{data.Cities.Count()} cites, {data.Cities.Sum(c => c.Population)} people";
+        }
+    }
+}
