0dee5ef6fdba2df694957a0cfe4eba9931ca90fa
Mikita Luksha <nikita.luksha@softensity.com>
2021-12-06 15:53:58 +0300

    Add Tr tag helper

---
+++ WebApp/WebApp/TagHelpers/TrTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApp.TagHelpers
+{
+    [HtmlTargetElement("tr", Attributes = "bg-color,text-color", ParentTag ="thead")]
+    public class TrTagHelper: TagHelper
+    {
+        public string BgColor { get; set; } = "dark";
+        public string TextColor { get; set; } = "white";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", $"bg-{BgColor} text-center text-{TextColor}");
+        }
+    }
+}
