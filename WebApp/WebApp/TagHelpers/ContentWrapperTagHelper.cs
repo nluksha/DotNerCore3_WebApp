6b5dd78055d66f3e0db502cf69375e37e11b6632
Nikita Luksha <nikita.luksha@softensity.com>
2021-12-06 20:29:33 +0300

    Add some tag helpers

---
+++ WebApp/WebApp/TagHelpers/ContentWrapperTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApp.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes ="[wrap=true]")]
+    public class ContentWrapperTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var el = new TagBuilder("div");
+            el.Attributes["class"] = "bg-primary text-white p-2 m-2";
+            el.InnerHtml.AppendHtml("Wrapper");
+
+            output.PreElement.AppendHtml(el);
+            output.PostElement.AppendHtml(el);
+        }
+    }
+}
---
+++ WebApp/WebApp/TagHelpers/HighlightTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApp.TagHelpers
+{
+    [HtmlTargetElement("*", Attributes = "[highlight=true]")]
+    public class HighlightTagHelper: TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.PreContent.SetHtmlContent("<b><i>");
+            output.PostContent.SetHtmlContent("</i></b>");
+        }
+    }
+}
