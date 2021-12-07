af6d61d07a8387645fa2d29cc89fea54c4fb9ef0
Nikita Luksha <nikita.luksha@softensity.com>
2021-12-07 08:34:06 +0300

    Refacrtor TagHelpers

---
+++ WebApp/WebApp/TagHelpers/TimeTagHelperComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApp.TagHelpers
+{
+    public class TimeTagHelperComponent: TagHelperComponent
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var timestamp = DateTime.Now.ToLongTimeString();
+
+            if (output.TagName == "body")
+            {
+                var el = new TagBuilder("div");
+                el.Attributes.Add("class", "bg-info text-white m-2 p-2");
+                el.InnerHtml.Append($"Time: {timestamp}");
+
+                output.PreContent.AppendHtml(el);
+            }
+        }
+    }
+}
