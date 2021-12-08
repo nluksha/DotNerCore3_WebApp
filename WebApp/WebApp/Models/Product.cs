16c32fc2578588eda9cd6c27e67f8747dfea951a
Nikita Luksha <nikita.luksha@softensity.com>
2021-12-08 20:23:04 +0300

    Add Model formating

--- WebApp/WebApp/Models/Product.cs
+++ WebApp/WebApp/Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApp.Models
@@ -11,6 +12,7 @@ namespace WebApp.Models
         public long ProductId { get; set; }
         public string Name { get; set; }
         [Column(TypeName = "decimal(8, 2)")]
+        [DisplayFormat(DataFormatString = "{0:c2}", ApplyFormatInEditMode = true)]
         public decimal Price { get; set; }
 
         public long CategoryId { get; set; }
