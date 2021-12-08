3160f9302f9b8f25fc3aa8c2e1e8d08d507d2896
Nikita Luksha <nikita.luksha@softensity.com>
2021-12-08 20:34:39 +0300

    Use label Tag helpers

--- WebApp/WebApp/Controllers/FormController.cs
+++ WebApp/WebApp/Controllers/FormController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -18,7 +19,12 @@ namespace WebApp.Controllers
 
         public async Task<IActionResult> Index(long id = 1)
         {
-            return View("Form", await context.Products.FindAsync(id));
+            var product = await context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .FirstAsync(p => p.ProductId == id);
+
+            return View("Form", product);
         }
 
         [HttpPost]
