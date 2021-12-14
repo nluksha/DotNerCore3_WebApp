ce251f99fbfe029d15ace6a6320ed7b8fb8fd19b
Nikita Luksha <nikita.luksha@softensity.com>
2021-12-14 09:16:27 +0300

    Product details

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -25,5 +25,17 @@ namespace WebApp.Controllers
         {
             return View(context.Products.Include(p => p.Category).Include(p => p.Supplier));
         }
+
+        public async Task<IActionResult> Details(long id)
+        {
+            Product p = await context.Products
+                .Include(p => p.Category)
+                .Include(p => p.Supplier)
+                .FirstOrDefaultAsync(p => p.ProductId == id);
+
+            ProductViewModel model = ViewModelFactory.Details(p);
+
+            return View("ProductEditor", model);
+        }
     }
 }
---
+++ WebApp/WebApp/Models/ViewModelFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public static class ViewModelFactory
+    {
+        public static ProductViewModel Details(Product p)
+        {
+            return new ProductViewModel
+            {
+                Product = p,
+                Action = "Details",
+                ReadOnly = true,
+                Theme = "info",
+                ShowAction = false,
+                Categories = p == null ? Enumerable.Empty<Category>() : new List<Category> { p.Category },
+                Suppliers = p == null ? Enumerable.Empty<Supplier>() : new List<Supplier> { p.Supplier }
+            };
+        }
+    }
+}
